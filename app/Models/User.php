70b853353660e91a5ad9b215e6dcbade795d90bd
xavizera <100section@gmail.com>
2025-04-25 15:49:38 -0300

    Add role attribute to User model and migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use App\Enums\UserRole;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -21,6 +23,15 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+    ];
+
+    /**
+     * Attribute values
+     *
+     * @return array<string, string>
+     */
+    protected $attributes = [
+        'role' => UserRole::User->value,
     ];
 
     /**
@@ -43,6 +54,7 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'role' => UserRole::class,
         ];
     }
 }
